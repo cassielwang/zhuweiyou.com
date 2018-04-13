eb1451f1fa83d458c14b53a169fe385c91982779
zhuweiyou <i@zhuweiyou.com>
2018-04-13 22:59:57 +0800

    move files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
-import Home from './Home'
+import Home from './pages/Home'
 
 export default () => (
   <BrowserRouter>
